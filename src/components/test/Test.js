dadd3aaf4cee2dc28d4762ecaae2495ebf42658b
Aml Fakhri <amlfakhry13@gmail.com>
2022-05-11 17:15:34 +0200

    know how to render multiple elemets

--- src/components/test/Test.js
+++ src/components/test/Test.js
@@ -1,4 +1,11 @@
+import React from 'react';
 import './Test.css';
-export default function Test() {
-  return <h1 className='testStyle'> HALLO REACT YA BADRO.</h1>;
+export default function Test(props) {
+  return (
+    <React.Fragment>
+      {props.names.map((name) => (
+        <h1 className='testStyle'>HALLO REACT YA {name}</h1>
+      ))}
+    </React.Fragment>
+  );
 }
