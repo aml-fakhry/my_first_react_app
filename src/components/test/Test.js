15c07bf7a465724a6a59e622549dc35c0c66d923
Aml Fakhri <amlfakhry13@gmail.com>
2022-05-11 17:32:42 +0200

    added a button (still use component as function)

--- src/components/test/Test.js
+++ src/components/test/Test.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import './Test.css';
 export default function Test(props) {
+  function pressed() {
+    console.log('pressed.');
+  }
+
   return (
     <React.Fragment>
       {props.names.map((name) => (
         <h1 className='testStyle'>HALLO REACT YA {name}</h1>
       ))}
+      <button className='changeClr' onClick={pressed}>
+        change Color
+      </button>
     </React.Fragment>
   );
 }
