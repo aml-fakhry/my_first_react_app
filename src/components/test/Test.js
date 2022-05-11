9777c20738c13b2dbb00a44fb16fc684e3a3a56a
Aml Fakhri <amlfakhry13@gmail.com>
2022-05-11 18:25:24 +0200

    use state in class copmonent.

--- src/components/test/Test.js
+++ src/components/test/Test.js
@@ -1,18 +1,34 @@
 import React from 'react';
 import './Test.css';
-export default function Test(props) {
-  function pressed() {
-    console.log('pressed.');
+export default class Test extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      color: 'red',
+    };
   }
 
-  return (
-    <React.Fragment>
-      {props.names.map((name) => (
-        <h1 className='testStyle'>HALLO REACT YA {name}</h1>
-      ))}
-      <button className='changeClr' onClick={pressed}>
-        change Color
-      </button>
-    </React.Fragment>
-  );
+  pressed = () => {
+    console.log('pressed.');
+    this.setState({
+      color: this.state.color === 'red' ? 'green' : 'red',
+    });
+  };
+
+  render() {
+    const colorClass = this.state.color;
+    const colorToggle = this.state.color === 'red' ? 'green' : 'red';
+    return (
+      <React.Fragment>
+        {this.props.names.map((name, idx) => (
+          <h1 key={idx} className={`testStyle ${colorClass}`}>
+            HALLO REACT YA {name}
+          </h1>
+        ))}
+        <button className='changeClr' onClick={this.pressed}>
+          {`change Color to ${colorToggle}`}
+        </button>
+      </React.Fragment>
+    );
+  }
 }
