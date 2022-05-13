c96328d8aa599a4652588cf6f457f32ce7acca48
Aml Fakhri <amlfakhry13@gmail.com>
2022-05-13 14:12:34 +0200

    componentWillUnmount

--- src/components/niceText/NiceText.js
+++ src/components/niceText/NiceText.js
@@ -1,12 +1,24 @@
 import React from 'react';
 export default class NiceText extends React.Component {
+  onClick = () => {
+    console.log(
+      'this log is because of the listener you added in the nice text component'
+    );
+  };
   componentDidMount() {
+    window.addEventListener('click', this.onClick);
     console.log('Nice Text is ready');
   }
 
   componentDidUpdate() {
     console.log('Nice Text updated');
   }
+
+  componentWillUnmount() {
+    window.removeEventListener('click', this.onClick);
+    console.log('Nice Text will be removed');
+  }
+
   render() {
     return (
       <h1 key={this.props.idx} className={`testStyle ${this.props.colorClass}`}>
